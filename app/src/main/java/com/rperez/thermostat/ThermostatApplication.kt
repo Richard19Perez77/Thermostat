b5cb0a9cd3e04ded5d7c8b7b4ed99a298ae8b32a
richa <richard19.perez77@yahoo.com>
2025-03-12 14:25:14 +0700

    formatting

--- app/src/main/java/com/rperez/thermostat/ThermostatApplication.kt
+++ app/src/main/java/com/rperez/thermostat/ThermostatApplication.kt
@@ -4,5 +4,5 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class ThermostatApplication: Application(){
+class ThermostatApplication : Application() {
 }
